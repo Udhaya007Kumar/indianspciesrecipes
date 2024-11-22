32e60700bedcbe9a237ddecdf96c6d8380f37c02
Udhaya007Kumar <udhaya93kumar@gmail.com>
2024-11-22 23:11:44 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,6 @@ import Profile from './Pages/Profile';
 import Profileedit from './Pages/Profileedit';
 import About from './Pages/About';
 import { Toaster } from "react-hot-toast";
-import { useQuery } from '@tanstack/react-query';
-import LoadingSpinner from "./Components/LoadingSpinner";
 import Navbar from './Components/Navbar';
 import SearchPage from './Pages/SearchPage';
 import UserRecipesList from './Pages/UserRecipesList';
@@ -23,70 +21,6 @@ import NotfoundPage from './Pages/NotfoundPage';
 
 
 const App = () => {
-
-  // const isAuthenticated = true; // Replace with your actual logic for checking authentication status.
-
-  // const { data: authUser, isLoading, error } = useQuery({
-  //   queryKey: ["authUser"],
-  //   queryFn: async () => {
-  //     const res = await fetch("http://localhost:4000/api/auth/user", {
-  //       method: "GET",
-  //       credentials: "include",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     });
-  //     const data = await res.json();
-  //     if (!res.ok || data.error) {
-  //       throw new Error(data.error || "Failed to fetch user data");
-  //     }
-  //     return data;
-  //   },
-  //   retry: false,
-  //   refetchOnWindowFocus: false,
-  //   refetchOnMount: false,
-  //   enabled: isAuthenticated, // Only run query if user is authenticated
-  // });
-  
-  // useEffect(() => {
-  //   if (!isLoading && authUser) {
-  //     console.log("Authenticated user:", authUser);
-  //   } else if (error) {
-  //     console.error("Error fetching auth user:", error.message);
-  //   }
-  // }, [authUser, isLoading, error]);
-
-
-  //  if(isLoading){
-  //    return <div className='flex justify-center items-center h-screen'>
-  //   <LoadingSpinner size='lg' />
-  //     </div>;
-  //  }
-
-
- 
-    
-
-
-  // const { user } = useContext(UserContext);
-
-  // console.log(user);
-  
-  
-  //  const { user } = useContext(UserContext);
-
-   
-
-
-
-   
-  
-
-
-
-
-
-
 
 
 
@@ -106,10 +40,13 @@ const App = () => {
           <Route path='/profile' element={  <Profile/> } />
           <Route path='/profile/:id' element={ <Profileedit/>} />
           <Route path='/search' element={ <SearchPage/> } />
+          
           <Route path='/userrecipes/:id' element={<UserRecipesList/>} />
+
+          <Route path='*' element={<NotfoundPage/>} />
           
           
-          <Route path='*' element={<Notfound/>} />
+         
         </Routes>
         <Toaster />
       </UserContextProvider>
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -32,7 +32,7 @@ const Homepage = () => {
   };
 
   return (
-    <div class="container mx-auto mt-8 px-4">
+    <div className="container mx-auto mt-8 px-4">
       <div className="flex flex-wrap gap-5 justify-center">
         {
           currentRecipes.map((recipe, index) => (
@@ -44,26 +44,26 @@ const Homepage = () => {
                   alt={recipe.title || "Recipe Image"}
                 />
               </Link>
-    <div class="p-4">
-      <h3 class="text-lg font-semibold text-gray-800">{recipe.title || "Untitled Recipe"}</h3>
-      <p class="text-gray-600 text-sm mt-1">
-      Ingredients: <span class="font-medium">{recipe.ingredients || "Recipe ingredients"}</span>
+    <div className="p-4">
+      <h3 className="text-lg font-semibold text-gray-800">{recipe.title || "Untitled Recipe"}</h3>
+      <p className="text-gray-600 text-sm mt-1">
+      Ingredients: <span className="font-medium">{recipe.ingredients || "Recipe ingredients"}</span>
       </p>
      
-      <p class="text-gray-600 text-sm mt-1">
-        Cooking Time: <span class="font-medium">{recipe.cookingTime || "Recipe cookingTime"}min</span>
+      <p className="text-gray-600 text-sm mt-1">
+        Cooking Time: <span className="font-medium">{recipe.cookingTime || "Recipe cookingTime"}min</span>
       </p>
-      <p class="text-gray-600 text-sm mt-1">
-        Servings: <span class="font-medium">{recipe.servings || "Recipe servings"}</span>
+      <p className="text-gray-600 text-sm mt-1">
+        Servings: <span className="font-medium">{recipe.servings || "Recipe servings"}</span>
       </p>
-      <div class="mt-4 flex justify-between items-center">
+      <div className="mt-4 flex justify-between items-center">
         <Link to={`/recipe/${recipe._id}`} >
-        <button class="text-white bg-blue-500 hover:bg-blue-600 px-3 py-2 rounded-md text-sm">
+        <button className="text-white bg-blue-500 hover:bg-blue-600 px-3 py-2 rounded-md text-sm">
           View Recipe
         </button>
         </Link>
        
-        <div class="flex items-center space-x-1 text-gray-600">
+        <div className="flex items-center space-x-1 text-gray-600">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-red-500" fill="currentColor" viewBox="0 0 24 24">
             <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
           </svg>
